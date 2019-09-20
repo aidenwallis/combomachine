9976e3aeab05f95790682a0a553ed2533d71f481
Aiden Wallis <aiden@aidenwallis.co.uk>
2019-09-20 21:54:44 +0100

    oops

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ if (!path[0]) {
 
 const query = {};
 if (window.location.search && window.location.search.length && window.location.search[0] === '?') {
-  const items = path[1].substring(1).split('&');
+  const items = window.location.search.substring(1).split('&');
   for (let i = 0; i < items.length; ++i) {
     const [name, value] = items[i].split('=');
     query[name] = value ? decodeURIComponent(value) : null;
